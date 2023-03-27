80485139395bfc2efe3fdb0b3f00d068ff398280
Rasam <rassamrl@gmail.com>
2023-03-27 17:44:49 +0300

    exc done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+function writeCards(list, event) {
+    let message_List = []
+    for (let i=0; i < list.length; i++) {
+        message_List.push( `Thank you, ${list[i]}, for the wonderful ${event} gift!`)
+    }
+    return message_List;
+}
+
+function countDown(int) {
+    let iterator = 0;
+    while( iterator <= int ) {
+        console.log(int)
+        int --
+    }
+}
+
